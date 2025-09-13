547924196caef8568fda94c04b85b3def276e213
kiahDev <208305271+kiahDev@users.noreply.github.com>
2025-09-13 21:49:36 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const talkBtn = document.getElementById('talk-btn');
+
+talkBtn.addEventListener('click', () => {
+    window.open("https://mail.google.com/mail/?view=cm&fs=1&to=hezekiahbrndn@gmail.com&su=Let's%20Talk&body=Hi%20Hezekiah,", "_blank");
+});
