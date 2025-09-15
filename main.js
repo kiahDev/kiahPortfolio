7428b27bcc24412084dca201df64ce23e893c361
kiahDev <208305271+kiahDev@users.noreply.github.com>
2025-09-16 07:24:25 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const talkBtn = document.getElementById('talk-btn');
 
 talkBtn.addEventListener('click', function() {
-    window.open("https://mail.google.com/mail/?view=cm&fs=1&to=hezekiahbrndn@gmail.com&su=Let's%20Talk&body=Hi%20Hezekiah,", "_blank");
+    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+        window.location.href = "mailto:hezekiahbrndn@gmail.com?subject=Let's Talk&body=Hi Hezekiah,";
+    } else {
+        window.open("https://mail.google.com/mail/?view=cm&fs=1&to=hezekiahbrndn@gmail.com&su=Let's%20Talk&body=Hi%20Hezekiah,", "_blank");
+    }
 });
-
-
-
